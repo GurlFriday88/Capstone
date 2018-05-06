2077e4476b26470c6c06c00c0e3f2d606014dc61
GurlFriday88 <entrotlek@gmail.com>
2018-05-06 19:17:35 -0400

    added db context , but ef is not working. working on fixing

---
+++ Capstone/Data/CapstoneDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Capstone.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Data
+{
+    public class CapstoneDBContext : DbContext
+    {
+        public DbSet<AutoMemo>AutoMemos  { get; set; }
+        public DbSet<MemoChoice>MemoChoices  { get; set; }
+        public DbSet<Provider>Providers  { get; set; }
+        public DbSet<BcbsPrefix>BcbsPrefixes  { get; set; }
+        public DbSet<Patient>Patients  { get; set; }
+        public DbSet<Store>Stores  { get; set; }
+
+public CapstoneDBContext(DbContextOptions<CapstoneDBContext> options)
+            : base(options)
+        {
+        }
+
+
+    }
+}

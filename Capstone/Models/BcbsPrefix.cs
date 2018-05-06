582d16c7f95141b9a544d4cd8d99cba8a269c5f9
GurlFriday88 <entrotlek@gmail.com>
2018-05-06 18:32:42 -0400

    Added Admin LTE template to project

---
+++ Capstone/Models/BcbsPrefix.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class BcbsPrefix
+    {
+        public int ID { get; set; }
+
+        public string Prefix { get; set; }
+
+        public int ProviderID { get; set; }
+
+        public Provider Provider { get; set; }
+
+    }
+}
--- Capstone/Startup.cs
+++ Capstone/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Capstone
+namespace OpticalOutletsHub
 {
     public class Startup
     {

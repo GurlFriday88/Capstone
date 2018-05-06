1ba6ca9336dbb902e4ad48299d452ee853f4c7b7
GurlFriday88 <entrotlek@gmail.com>
2018-05-06 11:15:58 -0400

    added store model

---
+++ Capstone/Models/Store.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class Store
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string PhoneNumeber { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public int ZipCode { get; set; }
+
+
+
+    }
+}

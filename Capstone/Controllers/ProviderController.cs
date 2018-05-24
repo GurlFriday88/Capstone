308213711a2e2229daaa8da3e026e9840c527bc2
GurlFriday88 <entrotlek@gmail.com>
2018-05-24 01:03:26 -0400

    added delete and add action results to provider controller

--- Capstone/Controllers/ProviderController.cs
+++ Capstone/Controllers/ProviderController.cs
@@ -2,32 +2,92 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Capstone.Data;
+using Capstone.Models;
+using Capstone.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Capstone.Controllers
 {
     public class ProviderController : Controller
     {
 
+        private CapstoneDBContext context;
+
+        public ProviderController(CapstoneDBContext dbContext)
+        {
+            context = dbContext;
+        }
+
         //Home for provider notes
         public IActionResult Index()
         {
-            return View();
+            IList<Provider> Providers = context.Providers.ToList();
+
+            return View(Providers);
         }
 
         //for detail view of note
 
-        public IActionResult Detail()
+        public IActionResult Detail(int id)
         {
-            return View();
+            Provider providerToView = context.Providers.SingleOrDefault(p => p.ID == id);
+            return View(providerToView);
         }
 
         //add for provider notes
-
+        //get provider note
         public IActionResult Add()
         {
-            return View();
+            AddProviderNoteViewModel addProviderViewModel = new AddProviderNoteViewModel(context.Providers.Include(p => p.Prefixes).Include(p => p.Patients).Include(p => p.Contact).ToList());
+            return View(addProviderViewModel);
+
         }
+
+        //post provider note
+        public IActionResult Add(AddProviderNoteViewModel addProviderViewModel)
+        {
+            //grab the checkbox or select items from view model and find their matches in db
+            IList<Prefix> selectedPrefixes =
+                   context.Prefixes.Where(p => p.ID == addProviderViewModel.SelectedProvider).ToList();
+
+            Contact contactItems = context.Contacts.FirstOrDefault(c => c.PhoneNumber == addProviderViewModel.PhoneNumber);
+
+            IList<Patient> selectedPatients = context.Patients.Where(p => p.ID == addProviderViewModel.SelectedPatient).ToList();
+            if (ModelState.IsValid)
+            {
+                // Add the new provider 
+                Provider newProvider = new Provider
+                {
+                    Name = addProviderViewModel.Name,
+                    SubscriberNumber = addProviderViewModel.SubscriberNumber,
+                    PagesToSave = addProviderViewModel.PagesToSave,
+                    SavedPagesDescription = addProviderViewModel.SavedPagesDescription,
+                    PhoneNumber = contactItems.PhoneNumber,
+                    Address = contactItems.Address,
+                    BenefitRenewal = addProviderViewModel.BenefitRenewal,
+                    AuthNote = addProviderViewModel.AuthNote,
+                    MiscNotes = addProviderViewModel.MiscNotes,
+                    Prefixes = selectedPrefixes,
+                    Patients = selectedPatients
+
+
+                };
+                context.Providers.Add(newProvider);
+                context.SaveChanges();
+                return Redirect("Detail");
+            }
+            return View("Add");
+
+
+        }
+
+
+
+
+
+
 
         //edit for provider notes
 
@@ -38,9 +98,22 @@ namespace Capstone.Controllers
 
         //delete for provider notes
 
-        public IActionResult Delete()
+        public IActionResult Delete(int id)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                Provider providerToDelete = context.Providers.SingleOrDefault(p => p.ID == id);
+                context.Providers.Remove(providerToDelete); //query to delete the user selected provider  
+
+                context.SaveChanges(); // commits changes to the db
+
+                IList<Provider> remainingProviders = context.Providers.ToList();
+
+                return View("ListProvider", remainingProviders);
+            }
+
+
+            return View("ListProvider");//send the user back to the list
         }
 
     }
--- Capstone/Models/Provider.cs
+++ Capstone/Models/Provider.cs
@@ -13,6 +13,10 @@ namespace Capstone.Models
        
         public string Name { get; set; }
 
+        public string PhoneNumber { get; set; }
+
+        public string Address { get; set; }
+
         public string SubscriberNumber { get; set; }
 
         

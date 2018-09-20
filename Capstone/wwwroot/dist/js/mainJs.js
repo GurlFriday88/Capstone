c7212336418eae0db593b013dfba75c0c649fc22
GurlFriday88 <entrotlek@gmail.com>
2018-09-20 00:36:18 -0400

    fixed table striping issue. centered contact tab box

---
+++ Capstone/wwwroot/dist/js/mainJs.js
@@ -0,0 +1,19 @@
+﻿
+//alternates row colors of table 
+function alternate() {
+
+    var row = document.getElementsByClassName("row_items");
+
+    for (i = 0; i < row_items.length; i++) {
+
+
+
+        if (i % 2 == 0) {
+
+            row[i].className = "even";
+        }else {
+            row[i].className = "odd";
+        }
+    }
+
+}
